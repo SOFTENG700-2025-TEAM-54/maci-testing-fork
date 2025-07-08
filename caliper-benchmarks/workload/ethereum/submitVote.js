5daf1a32cbc79777be07194dc31b8829b1bca1ed
ChrisKw0n <ckwo873@aucklanduni.ac.nz>
2025-07-08 16:34:24 +1200

    fix: adjust workload to correct input syntax

--- caliper-benchmarks/workload/ethereum/submitVote.js
+++ caliper-benchmarks/workload/ethereum/submitVote.js
@@ -1,20 +1,24 @@
 "use strict";
 
 const { WorkloadModuleBase } = require("@hyperledger/caliper-core");
+const Web3 = require("web3");
 
 class SimpleWorkload extends WorkloadModuleBase {
   async initializeWorkloadModule(workerIndex, totalWorkers, roundIndex, roundArguments, sutAdapter, sutContext) {
     await super.initializeWorkloadModule(workerIndex, totalWorkers, roundIndex, roundArguments, sutAdapter, sutContext);
+    this.web3 = new Web3(); // Local instance for ABI encoding
     console.log(`Worker ${workerIndex} ready`);
   }
 
   async submitTransaction() {
-    // Message struct: { data: uint256[10] }
+    // The contract expects:
+    // 1. tuple { data: uint256[10] }
+    // 2. tuple { x: uint256, y: uint256 }
+
     const message = {
       data: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
     };
 
-    // PublicKey struct: { x: uint256, y: uint256 }
     const publicKey = {
       x: "12345678901234567890",
       y: "98765432109876543210",
@@ -22,9 +26,9 @@ class SimpleWorkload extends WorkloadModuleBase {
 
     const request = {
       contract: "Poll",
-      readOnly: false,
       verb: "publishMessage",
       args: [message, publicKey],
+      readOnly: false,
     };
 
     return this.sutAdapter.sendRequests(request);
