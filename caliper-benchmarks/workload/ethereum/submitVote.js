a69c4284ec86e2b4a2083d052d5e9f50cacbcdcb
ChrisKw0n <ckwo873@aucklanduni.ac.nz>
2025-07-09 03:39:48 +1200

    feat: implemented workload using maci sdk

--- caliper-benchmarks/workload/ethereum/submitVote.js
+++ caliper-benchmarks/workload/ethereum/submitVote.js
@@ -1,34 +1,71 @@
 "use strict";
 
 const { WorkloadModuleBase } = require("@hyperledger/caliper-core");
-const Web3 = require("web3");
+const { Keypair, VoteCommand, Signature, PublicKey } = require("@maci-protocol/domainobjs");
 
 class SimpleWorkload extends WorkloadModuleBase {
   async initializeWorkloadModule(workerIndex, totalWorkers, roundIndex, roundArguments, sutAdapter, sutContext) {
     await super.initializeWorkloadModule(workerIndex, totalWorkers, roundIndex, roundArguments, sutAdapter, sutContext);
-    this.web3 = new Web3(); // Local instance for ABI encoding
+    this.user = new Keypair();
+
+    // Create a coordinator keypair and use its public key
+    // This ensures we have a properly formatted PublicKey object
+    const coordinatorKeypair = new Keypair();
+    this.coordinatorPubKey = coordinatorKeypair.publicKey;
+
+    // Alternative: if you need specific coordinates, create from raw values
+    // const x = BigInt("17392661484204959496580930106764958207212644848109387083812297145285218954");
+    // const y = BigInt("21089223111864377026700862467117778108729938232933496112109095249769709832");
+    // this.coordinatorPubKey = new PublicKey([x, y]);
+
+    console.log("coordinatorPubKey =", this.coordinatorPubKey);
+
+    this.pollId = 0;
     console.log(`Worker ${workerIndex} ready`);
   }
 
   async submitTransaction() {
-    // The contract expects:
-    // 1. tuple { data: uint256[10] }
-    // 2. tuple { x: uint256, y: uint256 }
+    // 1. Create the VoteCommand
+    const stateIndex = 1; // Use 1 instead of 0 - stateIndex 0 is usually reserved
+    const voteOptionIndex = 1n;
+    const voteWeight = 1n;
+    const nonce = 1n; // Start with nonce 1
 
-    const message = {
-      data: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
-    };
+    const command = new VoteCommand(
+      BigInt(stateIndex),
+      this.user.publicKey,
+      voteOptionIndex,
+      voteWeight,
+      nonce,
+      BigInt(this.pollId),
+    );
+    const signature = command.sign(this.user.privateKey);
 
-    const publicKey = {
-      x: "12345678901234567890",
-      y: "98765432109876543210",
-    };
+    // 2. Encrypt the command to create the message and ephemeral public key
+    const ecdhKeypair = new Keypair();
+    console.log("ecdhKeypair.privateKey =", ecdhKeypair.privateKey);
+    const sharedKey = Keypair.generateEcdhSharedKey(ecdhKeypair.privateKey, this.coordinatorPubKey);
 
+    const message = command.encrypt(signature, sharedKey);
+    const encryptionPublicKey = ecdhKeypair.publicKey;
+
+    // 3. Convert to contract format
+    const messageParam = message.asContractParam();
+    const publicKeyParam = encryptionPublicKey.asContractParam();
+
+    console.log("messageParam:", messageParam);
+    console.log("publicKeyParam:", publicKeyParam);
+
+    // 4. Prepare the request for Caliper with higher gas limit
     const request = {
       contract: "Poll",
       verb: "publishMessage",
-      args: [message, publicKey],
+      args: [messageParam, publicKeyParam],
       readOnly: false,
+      options: {
+        gas: 1000000, // Increased gas limit
+        gasPrice: "20000000000", // 20 gwei
+      },
     };
 
     return this.sutAdapter.sendRequests(request);
